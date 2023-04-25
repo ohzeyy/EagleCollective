7d26693f34b4c14dd803dfd8f4d7726c1594bb55
Zach Peter <peterz@bc.edu>
2023-04-25 13:44:53 -0400

    Initial Commit

---
+++ EagleCollective/EagleCollectiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  EagleCollectiveApp.swift
+//  EagleCollective
+//
+//  Created by Zach Peter on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct EagleCollectiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
